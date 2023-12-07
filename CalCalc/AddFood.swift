f6bb71c85ad34eb65379b0ad40c5768705d9f9ea
Max Goldberg <mgoldb16@u.rochester.edu>
2023-12-07 14:48:10 -0500

    Add foods

---
+++ CalCalc/AddFood.swift
@@ -0,0 +1,18 @@
+//
+//  AddFood.swift
+//  CalCalc
+//
+//  Created by Max Goldberg on 12/7/23.
+//
+
+import SwiftUI
+
+struct AddFood: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFood()
+}
